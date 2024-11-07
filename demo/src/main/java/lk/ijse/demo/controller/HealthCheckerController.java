4bb166d9d04f1fd877146210b70bb8a03fe52173
SandulRusara <sandulrusara8@gmail.com>
2024-11-08 00:34:20 +0530

    add health checker

--- demo/src/main/java/lk/ijse/demo/controller/HealthCheckerController.java
+++ demo/src/main/java/lk/ijse/demo/controller/HealthCheckerController.java
@@ -1,4 +1,14 @@
 package lk.ijse.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/health")
 public class HealthCheckerController {
+    @GetMapping
+    public String healthChecker(){
+        return "note controller is working";
+    }
 }
