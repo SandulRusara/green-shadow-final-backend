efbbd89b8db0a2d9e62d4e54f6cf4a2aeb11db17
SandulRusara <sandulrusara8@gmail.com>
2024-11-08 14:41:13 +0530

    properti file changes

--- demo/src/main/java/lk/ijse/demo/controller/HealthCheckerController.java
+++ demo/src/main/java/lk/ijse/demo/controller/HealthCheckerController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthCheckerController {
     @GetMapping
     public String healthChecker(){
-        return "note controller is working sanu";
+        return "note controller is working sanu boys";
     }
 }
--- demo/src/main/java/lk/ijse/demo/util/Regex.java
+++ demo/src/main/java/lk/ijse/demo/util/Regex.java
@@ -1,4 +1,5 @@
 package lk.ijse.demo.util;
 
 public class Regex {
+
 }
