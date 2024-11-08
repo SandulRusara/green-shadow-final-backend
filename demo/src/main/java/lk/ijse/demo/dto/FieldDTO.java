4c00e1405e698a3f1dd3f78164f28df9eef1b576
SandulRusara <sandulrusara8@gmail.com>
2024-11-08 19:50:43 +0530

    filed dto update

--- demo/src/main/java/lk/ijse/demo/dto/FieldDTO.java
+++ demo/src/main/java/lk/ijse/demo/dto/FieldDTO.java
@@ -1,4 +1,20 @@
 package lk.ijse.demo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.awt.*;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class FieldDTO {
+    private String fieldCode;
+    private String fieldName;
+    private Point fieldLocation;
+    private Double extentSizeOfTheField;
+    private String fieldImage1;
+    private String fieldImage2;
 }
