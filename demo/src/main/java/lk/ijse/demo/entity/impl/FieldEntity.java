a70dffff69f8b2aa25ec2f703f514755a8184876
SandulRusara <sandulrusara8@gmail.com>
2024-11-08 19:49:28 +0530

    filed entity update

--- demo/src/main/java/lk/ijse/demo/entity/impl/FieldEntity.java
+++ demo/src/main/java/lk/ijse/demo/entity/impl/FieldEntity.java
@@ -1,4 +1,27 @@
 package lk.ijse.demo.entity.impl;
 
-public class FieldEntity {
+import jakarta.persistence.*;
+import lk.ijse.demo.entity.SuperEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.awt.*;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "field")
+public class FieldEntity implements SuperEntity {
+    @Id
+    private String fieldCode;
+    private String fieldName;
+    private Point fieldLocation;
+    private Double extentSizeOfTheField;
+    @Lob
+    private String fieldImage1;
+    @Lob
+    private String fieldImage2;
 }
