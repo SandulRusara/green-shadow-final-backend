6bed05395304fcd55103fa1b78e0f853d2a2aa82
SandulRusara <sandulrusara8@gmail.com>
2024-11-13 13:34:26 +0530

    entity update

--- demo/src/main/java/lk/ijse/demo/entity/impl/LogEntity.java
+++ demo/src/main/java/lk/ijse/demo/entity/impl/LogEntity.java
@@ -10,7 +10,7 @@ import java.util.List;
 @NoArgsConstructor
 @Data
 @Entity
-@Table(name = "monitoringLogServices")
+@Table(name = "logs")
 public class LogEntity {
     @Id
     private String logCode;
