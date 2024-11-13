a3c921f95ec1f463210be22b7b3b9e1db5443632
SandulRusara <sandulrusara8@gmail.com>
2024-11-13 23:03:01 +0530

    exception add

---
+++ demo/src/main/java/lk/ijse/demo/exception/FieldNotFoundException.java
@@ -0,0 +1,13 @@
+package lk.ijse.demo.exception;
+
+public class FieldNotFoundException {
+    public FieldNotFoundException() {
+
+    }
+
+    public FieldNotFoundException(String message) {
+    }
+
+    public FieldNotFoundException(String message, Throwable cause) {
+    }
+}
