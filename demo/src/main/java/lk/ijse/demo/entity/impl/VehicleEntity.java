1c2b0f9e039727d849b2f888fec8ba31b72bbc63
SandulRusara <sandulrusara8@gmail.com>
2024-11-13 13:29:59 +0530

    vehicle entity update

--- demo/src/main/java/lk/ijse/demo/entity/impl/VehicleEntity.java
+++ demo/src/main/java/lk/ijse/demo/entity/impl/VehicleEntity.java
@@ -15,15 +15,12 @@ import lombok.NoArgsConstructor;
 public class VehicleEntity {
     @Id
     private String vehicleCode;
-
     private String licensePlateNumber;
-    private String vehicleCategory;
-    private FuelType fuelType;
-    private String status;
-
-    @OneToOne
-    @JoinColumn(name = "allocatedStaffMemberDetail")
-    private StaffEntity allocatedStaffMemberDetails;
-
-    private String remarks;
+    private String Name;
+    private String category;
+    private String fuelType;
+    private String remark;
+    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinColumn(name = "memberCode")
+    private StaffEntity staff;
 }
