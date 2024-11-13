2fb60e3c12df9e2ce1ef1e2ed496d6c9080ab43a
SandulRusara <sandulrusara8@gmail.com>
2024-11-13 22:53:42 +0530

    exception add

---
+++ demo/src/main/java/lk/ijse/demo/exception/CropNotFoundException.java
@@ -0,0 +1,13 @@
+package lk.ijse.demo.exception;
+
+public class CropNotFoundException extends RuntimeException{
+    public CropNotFoundException() {
+
+    }
+
+    public CropNotFoundException(String message) {
+    }
+
+    public CropNotFoundException(String message, Throwable cause) {
+    }
+}
