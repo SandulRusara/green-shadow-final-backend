6e65b1b851fa4f2f38f3d1781bbda99e94952ca9
SandulRusara <sandulrusara8@gmail.com>
2024-11-15 19:15:44 +0530

    crop sevice impl update

--- demo/src/main/java/lk/ijse/demo/service/impl/CropServiceImpl.java
+++ demo/src/main/java/lk/ijse/demo/service/impl/CropServiceImpl.java
@@ -1,8 +1,13 @@
 package lk.ijse.demo.service.impl;
 
+import jakarta.transaction.TransactionScoped;
+import jakarta.transaction.Transactional;
 import lk.ijse.demo.dto.impl.CropDTO;
 import lk.ijse.demo.service.CropService;
+import org.springframework.stereotype.Service;
 
+@Service
+@Transactional
 public class CropServiceImpl implements CropService {
     @Override
     public void saveCrop(CropDTO cropDTO) {
