418bca96db5726d1246fbb7e7ad065495a1643cd
SandulRusara <sandulrusara8@gmail.com>
2024-11-15 19:10:27 +0530

    crop controller anotztion add

--- demo/src/main/java/lk/ijse/demo/controller/CropController.java
+++ demo/src/main/java/lk/ijse/demo/controller/CropController.java
@@ -1,4 +1,11 @@
 package lk.ijse.demo.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/crops")
+@CrossOrigin
 public class CropController {
 }
