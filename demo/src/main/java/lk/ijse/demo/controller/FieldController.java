e8109f3a437cfd986b47abee55572843a58ebecd
SandulRusara <sandulrusara8@gmail.com>
2024-11-15 09:40:53 +0530

    anotation add and dependancy injection add

--- demo/src/main/java/lk/ijse/demo/controller/FieldController.java
+++ demo/src/main/java/lk/ijse/demo/controller/FieldController.java
@@ -1,5 +1,7 @@
 package lk.ijse.demo.controller;
 
+import lk.ijse.demo.service.FieldService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,5 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/field")
 @CrossOrigin
 public class FieldController {
+    @Autowired
+    private FieldService fieldService;
+
 
 }
