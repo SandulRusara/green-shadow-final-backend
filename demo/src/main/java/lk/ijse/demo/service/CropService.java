545f1547adf8219798c317bb1f2964a1d3692cc0
SandulRusara <sandulrusara8@gmail.com>
2024-11-15 19:12:34 +0530

    crop dao update

---
+++ demo/src/main/java/lk/ijse/demo/service/CropService.java
@@ -0,0 +1,7 @@
+package lk.ijse.demo.service;
+
+import lk.ijse.demo.dto.impl.CropDTO;
+
+public interface CropService {
+    void saveCrop(CropDTO cropDTO);
+}
---
+++ demo/src/main/java/lk/ijse/demo/service/impl/CropServiceImpl.java
@@ -0,0 +1,4 @@
+package lk.ijse.demo.service.impl;
+
+public class CropServiceImpl {
+}
