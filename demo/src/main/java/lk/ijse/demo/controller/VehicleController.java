5509acdb8d260edd98df156cf2f8a472a899486c
SandulRusara <sandulrusara8@gmail.com>
2024-11-22 16:48:11 +0530

    create vehicle controller save method

--- demo/src/main/java/lk/ijse/demo/controller/VehicleController.java
+++ demo/src/main/java/lk/ijse/demo/controller/VehicleController.java
@@ -1,5 +1,30 @@
 package lk.ijse.demo.controller;
 
-public class VehicleController {
+import lk.ijse.demo.dto.impl.VehicleDTO;
+import lk.ijse.demo.exception.DataPersistException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("api/v1/vehicles")
+@CrossOrigin
+public class VehicleController {
+    @Autowired
+    private VehicleService vehicleService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveVehicle(@RequestBody VehicleDTO vehicleDTO){
+        try{
+            vehicleService.saveVehicle(vehicleDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }catch (DataPersistException e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
