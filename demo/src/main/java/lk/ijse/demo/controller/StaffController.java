b75db5ea4563d71305b74f190ad6f72cee36e033
SandulRusara <sandulrusara8@gmail.com>
2024-11-23 22:56:20 +0530

    staff controller crated

---
+++ demo/src/main/java/lk/ijse/demo/controller/StaffController.java
@@ -0,0 +1,31 @@
+package lk.ijse.demo.controller;
+
+import lk.ijse.demo.dto.impl.StaffDTO;
+import lk.ijse.demo.exception.DataPersistException;
+import lk.ijse.demo.util.Regex;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/staff")
+@CrossOrigin
+public class StaffController {
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveStaff(@RequestBody StaffDTO staffDTO){
+        try{
+            if (!Regex.emailValidator(staffDTO.getEmail()).matches()){
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+            staffService.saveStaffMember(staffDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }catch (DataPersistException e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
