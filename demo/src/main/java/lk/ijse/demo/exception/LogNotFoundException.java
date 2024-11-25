b7dd97d15e94aed1c2c12118d909e998cd6f70c5
SandulRusara <sandulrusara8@gmail.com>
2024-11-25 11:24:48 +0530

    add log exseption

---
+++ demo/src/main/java/lk/ijse/demo/exception/LogNotFoundException.java
@@ -0,0 +1,13 @@
+package lk.ijse.demo.exception;
+
+public class LogNotFoundException {
+    public LogNotFoundException() {
+
+    }
+
+    public LogNotFoundException(String message) {
+    }
+
+    public LogNotFoundException(String message, Throwable cause) {
+    }
+}
